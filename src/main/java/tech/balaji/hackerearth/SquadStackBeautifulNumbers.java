b7602e4eb95c4f9b6ca7e10b495d3ad1653e5e20
balaji043 <cubanhog.royalgamma@gmail.com>
2021-01-16 22:54:10 +0530

    added edge test case

--- src/main/java/tech/balaji/hackerearth/SquadStackBeautifulNumbers.java
+++ src/main/java/tech/balaji/hackerearth/SquadStackBeautifulNumbers.java
@@ -30,22 +30,34 @@ public class SquadStackBeautifulNumbers {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 4, 5};
-        int k = 1;
-        System.out.println(solve(arr, k));
+        int[] a = {1, 2, 3, 4, 5};
+        int aK = 1;
+        System.out.println(solve(a, aK));
 
-        int[] a = {4, 3, 4, 3, 4};
-        int n = 2;
-        System.out.println(solve(a, n));
+        int[] b = {4, 3, 4, 3, 4};
+        int bK = 2;
+        System.out.println(solve(b, bK));
+
+        int[] c = {3, 3, 3, 3, 3,};
+        int cK = 2;
+        System.out.println(solve(c, cK));
+
     }
 
     static int solve(int[] arr, int k) {
         minimum = Integer.MAX_VALUE;
         List<Number> numbers = new ArrayList<>();
-        for (int i : arr)
-            numbers.add(new Number(i));
+        int beautifulNumberCount = 0;
+        for (int i : arr) {
+            Number number = new Number(i);
+            numbers.add(number);
+            if (number.isBeautiful)
+                beautifulNumberCount++;
+        }
+        if (beautifulNumberCount == 0)
+            return -1;
         printSubArrays(numbers, 0, 0, k);
-        return minimum;
+        return minimum == Integer.MAX_VALUE ? -1 : minimum;
     }
 
     // Recursive function to print all possible subarrays
@@ -58,12 +70,12 @@ public class SquadStackBeautifulNumbers {
             printSubArrays(arr, 0, end + 1, k);
         else {
             int beautifulNumberCount = 0;
-            int subArrayCount = 0;
-            for (int i = start; i <= end; i++) {
+            int subArrayCount = end - start + 1;
+
+            for (int i = start; i <= end; i++)
                 if (arr.get(i).isBeautiful)
                     beautifulNumberCount++;
-                subArrayCount++;
-            }
+
             if (subArrayCount < minimum && beautifulNumberCount >= k)
                 minimum = subArrayCount;
 
