bff4f34602481c6b6661b177d63a6459d5136150
balaji043 <cubanhog.royalgamma@gmail.com>
2021-01-17 22:08:33 +0530

    moved list node to inner class

--- src/main/java/tech/balaji/leetcode/problems/ReverseSinglyLinkedList.java
+++ src/main/java/tech/balaji/leetcode/problems/ReverseSinglyLinkedList.java
@@ -41,19 +41,20 @@ public class ReverseSinglyLinkedList {
         }
         return prev;
     }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
